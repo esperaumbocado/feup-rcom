4ac5a3dedef2e88cd6b2d4fe7a9b05ba63a2e6d5
Manjeri998 <nunopinhofernandes@gmail.com>
2024-10-19 18:01:40 +0100

    control and data packet

--- Project1/src/application_layer.c
+++ Project1/src/application_layer.c
@@ -2,6 +2,12 @@
 
 #include "application_layer.h"
 
+#define CONTROL_FIELD_DATA 2
+#define CONTROL_FIELD_START 1
+#define CONTROL_FIELD_END 3
+#define TYPE_FILE_SIZE 0
+#define TYPE_FILE_NAME 1
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
@@ -25,6 +31,11 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
             "====================================\n",
             fd);
     }
+    switch (link_layer_info.role) {
+        case LlTx:
+
+        case LlRx:
+    }
     
 
     // Create a buffer and test sending it
@@ -41,4 +52,39 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     }
 
     llclose(0);
+}
+
+// Build the data packet to be sent
+unsigned char buildDataPacket(unsigned char sequence_number, unsigned char packet_data, int data_size) { 
+    unsigned char* packet = 4 + data_size;
+    packet[0] = CONTROL_FIELD_DATA;
+    packet[1] = sequence_number;
+    packet[2] = (data_size >> 8) && 0xFF, 
+    packet[3] = data_size && 0xFF;
+    memccpy(packet+4, packet_data, data_size);
+    return packet;
+}
+
+unsigned char buildControlPacket(int control_field, int L1, int length, int L2, char* filename) {
+    int ind = 0;
+    unsigned char* packet = 5 + L1 + L2;
+    packet[ind] = control_field;
+    ind++;
+    packet[ind] = TYPE_FILE_SIZE;
+    ind++;
+    packet[ind] = L1;
+    ind++;
+    for(int i = 0; i < L1, i++) {
+        packet[ind] = (length >> 8 * (L1 - i - 1)) && 0xFF;
+        ind++;
+    }
+    packet[ind] = TYPE_FILE_NAME;
+    ind++;
+    packet[ind] = L2;
+    ind++;
+    for(int i = 0; i < L2, i++) {
+        packet[ind] = (filename >> 8 * (L2 - i - 1)) && 0xFF;
+        ind++;
+    }
+    return packet;
 }
