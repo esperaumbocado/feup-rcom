6f151c7da3440a49da712f7aa8362b42105fca2e
Rui Borges <ruiborges206@gmail.com>
2024-12-15 00:37:31 +0000

    Made it a state machine

--- Project2/download.c
+++ Project2/download.c
@@ -8,65 +8,130 @@
 #include <unistd.h>
 
 
+typedef enum {
+    STATE_INIT,
+    STATE_PARSE_URL,
+    STATE_CONNECT_CONTROL,
+    STATE_AUTHENTICATE,
+    STATE_ENTER_PASSIVE,
+    STATE_CONNECT_DATA,
+    STATE_REQUEST_RESOURCE,
+    STATE_DOWNLOAD_RESOURCE,
+    STATE_CLOSE_CONNECTION,
+    STATE_SUCCESS,
+    STATE_ERROR
+} State;
+
 int main(int argc, char *argv[]) {
-
-    if (argc != 2) {
-        printf("Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
-        exit(-1);
-    } 
-
+    State currentState = STATE_INIT;
     char host[MAX_LENGTH], resource[MAX_LENGTH], file[MAX_LENGTH], user[MAX_LENGTH], password[MAX_LENGTH], ip[MAX_LENGTH];
+    char ip_pass[MAX_LENGTH], answer[MAX_LENGTH];
+    int socketA = -1, socketB = -1, port = 0;
 
+    while (currentState != STATE_SUCCESS && currentState != STATE_ERROR) {
+        switch (currentState) {
+            case STATE_INIT:
+                if (argc != 2) {
+                    printf("Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_PARSE_URL;
+                }
+                break;
 
-    if (parse(argv[1], host, resource, file, user, password, ip) != 0) {
-        printf("Parse error. Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
-        exit(-1);
-    }
-    
-    printf("-------------------\n URL INFO\n-------------------\nHost: %s\nResource: %s\nFile: %s\nUser: %s\nPassword: %s\nIP Address: %s\n-------------------\n", host, resource, file, user, password, ip);
+            case STATE_PARSE_URL:
+                if (parse(argv[1], host, resource, file, user, password, ip) != 0) {
+                    printf("Parse error. Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
+                    currentState = STATE_ERROR;
+                } else {
+                    printf("-------------------\n URL INFO\n-------------------\nHost: %s\nResource: %s\nFile: %s\nUser: %s\nPassword: %s\nIP Address: %s\n-------------------\n",
+                           host, resource, file, user, password, ip);
+                    currentState = STATE_CONNECT_CONTROL;
+                }
+                break;
 
-    char answer[MAX_LENGTH];
-    int socketA = createSocket(ip, FTP_PORT);
-    if (socketA < 0 || readResponse(socketA, answer) != READY4AUTH_CODE) {
-        printf("Socket to '%s' and port %d failed\n", ip, FTP_PORT);
-        exit(-1);
-    }
-    
-    if (authFTP(socketA, user, password) != LOGINSUCCESS_CODE) {
-        printf("Authentication failed with username = '%s' and password = '%s'.\n", user, password);
-        exit(-1);
-    }
-    
-    int port;
-    char ip_pass[MAX_LENGTH];
-    if (enterPassiveMode(socketA, ip_pass, &port) != PASSIVE_CODE) {
-        printf("Passive mode failed\n");
-        exit(-1);
+            case STATE_CONNECT_CONTROL:
+                socketA = createSocket(ip, FTP_PORT);
+                if (socketA < 0 || readResponse(socketA, answer) != READY4AUTH_CODE) {
+                    printf("Socket to '%s' and port %d failed\n", ip, FTP_PORT);
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_AUTHENTICATE;
+                }
+                break;
+
+            case STATE_AUTHENTICATE:
+                if (authFTP(socketA, user, password) != LOGINSUCCESS_CODE) {
+                    printf("Authentication failed with username = '%s' and password = '%s'.\n", user, password);
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_ENTER_PASSIVE;
+                }
+                break;
+
+            case STATE_ENTER_PASSIVE:
+                if (enterPassiveMode(socketA, ip_pass, &port) != PASSIVE_CODE) {
+                    printf("Passive mode failed\n");
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_CONNECT_DATA;
+                }
+                break;
+
+            case STATE_CONNECT_DATA:
+                socketB = createSocket(ip_pass, port);
+                if (socketB < 0) {
+                    printf("Socket to '%s:%d' failed\n", ip_pass, port);
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_REQUEST_RESOURCE;
+                }
+                break;
+
+            case STATE_REQUEST_RESOURCE:
+                {
+                    int requestResourceValue = requestResource(socketA, resource);
+                    printf("REQUEST RESOURCE VALUE %d\n", requestResourceValue);
+                    if (requestResourceValue != READY4TRANSFER_CODE && requestResourceValue != READY4TRANSFER_CODE2) {
+                        printf("Unknown resource '%s' in '%s:%d'\n", resource, ip_pass, port);
+                        currentState = STATE_ERROR;
+                    } else {
+                        currentState = STATE_DOWNLOAD_RESOURCE;
+                    }
+                }
+                break;
+
+            case STATE_DOWNLOAD_RESOURCE:
+                if (getResource(socketA, socketB, file) != TRANSFER_COMPLETE_CODE) {
+                    printf("Error transferring file '%s' from '%s:%d'\n", file, ip_pass, port);
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_CLOSE_CONNECTION;
+                }
+                break;
+
+            case STATE_CLOSE_CONNECTION:
+                if (closeConnection(socketA) != 0) {
+                    printf("Sockets close error\n");
+                    currentState = STATE_ERROR;
+                } else {
+                    currentState = STATE_SUCCESS;
+                }
+                break;
+
+            default:
+                currentState = STATE_ERROR;
+                break;
+        }
     }
 
-    int socketB = createSocket(ip_pass, port);
-    if (socketB < 0) {
-        printf("Socket to '%s:%d' failed\n", ip_pass, port);
-        exit(-1);
+    if (currentState == STATE_ERROR) {
+        if (socketA >= 0) close(socketA);
+        if (socketB >= 0) close(socketB);
+        printf("An error occurred during the FTP download.\n");
+        return -1;
     }
 
-    
-    int requestResourceValue = requestResource(socketA,resource);
-    printf("REQUEST RESOURCE VALUE %d\n", requestResourceValue);
-    if (requestResourceValue != READY4TRANSFER_CODE && requestResourceValue != READY4TRANSFER_CODE2) {
-        printf("Unknown resouce '%s' in '%s:%d'\n", resource, ip_pass, port);
-        exit(-1);
-    }
-
-    if (getResource(socketA, socketB, file) != TRANSFER_COMPLETE_CODE) {
-        printf("Error transfering file '%s' from '%s:%d'\n", file, ip_pass, port);
-        exit(-1);
-    }
-
-    if (closeConnection(socketA) != 0) {
-        printf("Sockets close error\n");
-        exit(-1);
-    }
-
+    printf("FTP download completed successfully.\n");
     return 0;
 }
